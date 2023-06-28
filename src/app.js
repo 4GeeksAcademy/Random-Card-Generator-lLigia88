831f7de70cd3683eec2186b3a9bf393fd299dd52
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-06-28 23:35:28 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -1,74 +1,53 @@
-const suits = ["hearts", "diamonds", "spades", "clubs"];
-const values = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K"
-];
-let suitTop = document.querySelector("#top-symbol");
-let number = document.querySelector("#number");
-let suitBottom = document.querySelector("#bottom-symbol");
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
 
-// Function to choose a random card from the given arrays
-function chooseCard(arr1, arr2) {
-  let card = [];
-  card.push(arr1[Math.floor(Math.random() * arr1.length)]);
-  card.push(arr2[Math.floor(Math.random() * arr2.length)]);
-  return card;
-}
-
-// Function to assign color based on the suit of the card
-function assignColor(arr) {
-  if (arr[0] === "hearts" || arr[0] === "diamonds") {
-    suitTop.style.color = "red";
-    number.style.color = "red";
-    suitBottom.style.color = "red";
-  } else {
-    suitTop.style.color = "black";
-    number.style.color = "black";
-    suitBottom.style.color = "black";
-  }
-}
-
-// Function to assign suit symbols to the card
-function assignSuit(arr) {
-  if (arr[0] === "hearts") {
-    suitTop.innerHTML = "&hearts;";
-    suitBottom.innerHTML = "&hearts;";
-  } else if (arr[0] === "diamonds") {
-    suitTop.innerHTML = "&diams;";
-    suitBottom.innerHTML = "&diams;";
-  } else if (arr[0] === "spades") {
-    suitTop.innerHTML = "&spades;";
-    suitBottom.innerHTML = "&spades;";
-  } else {
-    suitTop.innerHTML = "&clubs;";
-    suitBottom.innerHTML = "&clubs;";
-  }
-}
-
-// Function to assign card value to the number element
-function assignValue(arr) {
-  number.innerHTML = arr[1];
-}
-
-// Function to generate a new random card
-function generateCard() {
-  let card = chooseCard(suits, values);
-  assignColor(card);
-  assignSuit(card);
-  assignValue(card);
-}
-
+// Function to be executed when the window finishes loading
 window.onload = function() {
-  generateCard();
+  // Interval function to generate random card values every 10 seconds
+  setInterval(() => {
+    // Arrays for card icons and content
+    const icons = ["♦", "♥", "♠", "♣"];
+    const content = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+      "A"
+    ];
+
+    // Generate random indices for icons and content arrays
+    let randomIcon = Math.floor(Math.random() * icons.length);
+    let randomNumber = Math.floor(Math.random() * content.length);
+
+    // Get elements by their IDs
+    let firstIcon = document.getElementById("firstIcon");
+    let number = document.getElementById("number");
+    let secondIcon = document.getElementById("secondIcon");
+
+    // Assign random values to the card elements
+    firstIcon.innerHTML = icons[randomIcon];
+    number.innerHTML = content[randomNumber];
+    secondIcon.innerHTML = icons[randomIcon];
+
+    // Set color of icons based on specific conditions
+    if (
+      (firstIcon.innerHTML == icons[0] && secondIcon.innerHTML == icons[0]) ||
+      (firstIcon.innerHTML == icons[1] && secondIcon.innerHTML == icons[1])
+    ) {
+      firstIcon.style.color = "red";
+      secondIcon.style.color = "red";
+    } else {
+      firstIcon.style.color = "black";
+      secondIcon.style.color = "black";
+    }
+  }, 10000); // Execute every 10 seconds
 };
